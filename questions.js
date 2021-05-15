71b8442805288f03fa32f2d8859e24230c0c5182
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 21:02:40 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Surely you're not counting on him for help. He's totally ...............    .",
+        options:["unreliable", "uncountable", "untidy", "dependable", "affectionate"],
+        answer: 0
+    },
+    {
+        q:"A cardiologist tried all he could to reduce the patient's ............... pressure.",
+        options:["cell", "blood", "brain", "tissue", "bone"],
+        answer: 1
+    },
+    {
+        q:"............... of Black Death were not infrequent in the Middle Ages.",
+        options:["Illnesses", "Diseases", "Epidemics", "Paramedics", "Operations"],
+        answer: 2
+    },
+    {
+        q:"We were caught in the ...............-hour trafic.",
+        options:["rush", "bush", "push", "hurry", "emergency"],
+        answer: 0
+    }
+]
